d0ba56211e9c97876c027c34f1494ae7e1654ff6
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-12-06 08:39:27 +0200

    Simpler solution for 2022-6

--- 2022/6/common.go
+++ 2022/6/common.go
@@ -5,53 +5,26 @@ import (
 	"sort"
 )
 
-func GetAllSubstrs(input string, length int, outCh chan<- string, quitCh <-chan struct{}) {
-	for i := 0; i < len(input)-length; i++ {
-		select {
-		case <-quitCh:
-			return
-		default:
-			substr := input[i : i+length]
-			if len(substr) < length {
-				break
-			}
-			outCh <- substr
-		}
-	}
-}
-
-func FindFirstUnique(inCh <-chan string) int {
-	i := 0
-	for substr := range inCh {
-		var (
-			found = false
-			s     = []rune(substr)
-		)
-		sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
-		for i := 1; i < len(s); i++ {
-			if s[i] == s[i-1] {
-				found = false
-				break
-			} else {
-				found = true
-			}
-		}
-		if found {
-			return i + len(substr)
-		}
-		i++
-	}
-	return 0
-}
-
 func Solve(input string, length int) string {
-	c := make(chan string, 10)
-	quitCh := make(chan struct{})
+	for i := 0; i < len(input)-length; i++ {
+		substr := input[i : i+length]
+		if len(substr) < length {
+			break
+		}
+		if IsUnique(substr) {
+			return fmt.Sprint(i + len(substr))
+		}
+	}
+	return "Failed to solve"
+}
 
-	go GetAllSubstrs(input, length, c, quitCh)
-
-	i := FindFirstUnique(c)
-	quitCh <- struct{}{}
-
-	return fmt.Sprint(i)
+func IsUnique(input string) bool {
+	s := []rune(input)
+	sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			return false
+		}
+	}
+	return true
 }
