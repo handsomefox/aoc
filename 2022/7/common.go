62b2cdfc691dadc7693cdf8c7ebb5b2f7241f48f
Eduard Diachenko <handsomefox.hf@gmail.com>
2022-12-07 10:51:24 +0200

    Fixup 2022-7

--- 2022/7/common.go
+++ 2022/7/common.go
@@ -31,6 +31,62 @@ func ScanLs(sc *bufio.Scanner) []string {
 	return lsOutput
 }
 
+func cd(input string, current *FolderNode) *FolderNode {
+	input = input[5:]
+	switch input {
+	// "/" is a special case, because it is the first command encountered and the folder
+	// doesn't exist at this point.
+	case "/":
+		node := &FolderNode{
+			parent:   current,
+			name:     input,
+			children: make([]Node, 0),
+		}
+		current.children = append(current.children, node)
+		return node
+	case "..":
+		return current.Parent()
+	default:
+		for _, v := range current.children { // we find the specified folder inside the current one.
+			if v.Name() == input && v.Type() == NodeTypeFolder {
+				return v.(*FolderNode)
+			}
+		}
+	}
+	panic("unreachable") // unless the input has a mistake, this is unreachable, assume it doesn't
+}
+
+func ls(input []string, current *FolderNode) {
+	for _, str := range input {
+		if strings.HasPrefix(str, "dir") {
+			folder := parseFolder(str, current)
+			current.children = append(current.children, folder)
+			continue
+		}
+		file := parseFile(str, current)
+		current.children = append(current.children, file)
+	}
+}
+
+func parseFolder(input string, parent *FolderNode) *FolderNode {
+	foldername := strings.Split(input, " ")[1]
+	return &FolderNode{
+		parent:   parent,
+		name:     foldername,
+		children: make([]Node, 0),
+	}
+}
+
+func parseFile(input string, parent *FolderNode) *FileNode {
+	split := strings.Split(input, " ")
+	fileSize, fileName := MustParseInt(split[0]), split[1]
+	return &FileNode{
+		parent: parent,
+		name:   fileName,
+		sz:     fileSize,
+	}
+}
+
 type FileNode struct {
 	parent *FolderNode
 	name   string
